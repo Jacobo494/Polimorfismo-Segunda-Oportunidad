f5d4ae2873a8df03dc0d4e64a647cf7e82d1f34f
Alberto Jacobo Zamudio Chan <23470290@campeche.tecnm.mx>
2024-06-05 22:26:15 -0600

    Proporciona una implementación básica para las cuentas bancarias.

---
+++ bancoPolimorfimo.ConsoleApp/ClaseBase.cs
@@ -0,0 +1,21 @@
+﻿namespace bancoPolimorfimo.ConsoleApp
+{
+    public abstract class ClaseBase : CuentaDeBanco
+    {
+        private decimal saldo;
+
+        public decimal Saldo
+        {
+            get { return saldo; }
+            protected set { saldo = value; }
+        }
+
+        public void Depositar(decimal cantidad)
+        {
+            if (cantidad > 0)
+                Saldo += cantidad;
+        }
+
+        public abstract void Retirar(decimal cantidad);
+    }
+}
