17701b03fe31c27306caa280c1c3909b07f4c051
Alberto Jacobo Zamudio Chan <23470290@campeche.tecnm.mx>
2024-06-05 23:53:11 -0600

    Cuenta de cheques.

---
+++ bancoPolimorfimo.ConsoleApp/Cheques.cs
@@ -0,0 +1,43 @@
+﻿namespace bancoPolimorfismo.ConsoleApp
+{
+    public class Cheques : ClaseBase
+    {
+        private const decimal SaldoMinimo = 5000;
+        private const decimal InteresAnual = 0.02m;
+        private const int MaximoDeCheques = 5;
+        private const decimal CargoPorCheque = 10;
+        private const decimal CargoPorChequeRebotado = 200;
+
+        private int chequesEmitidos;
+
+        public Cheques(decimal saldoI)
+        {
+            if (saldoI >= SaldoMinimo)
+                this.Saldo = saldoI;
+        }
+
+        public override void Retirar(decimal cantidad)
+        {
+            if (cantidad <= Saldo)
+                this.Saldo -= cantidad;
+        }
+
+        public void EmitirCheque(decimal cantidad)
+        {
+            if (chequesEmitidos < MaximoDeCheques)
+            {
+                chequesEmitidos++;
+                Retirar(cantidad);
+            }
+            else
+            {
+                Saldo -= cantidad + CargoPorChequeRebotado;
+            }
+        }
+
+        public decimal CalcularInteresMensual()
+        {
+            return Saldo * InteresAnual / 12;
+        }
+    }
+}
--- bancoPolimorfimo.ConsoleApp/Program.cs
+++ bancoPolimorfimo.ConsoleApp/Program.cs
@@ -1,2 +1,18 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using bancoPolimorfismo.ConsoleApp;
+
+
+Console.WriteLine("Aplicacion el concepto de polimorfismo para la definición de clases abstractas e interfaces que permitan reutilización de código.");
+var cuentaBasica = new CuentaBasica(300);
+var cuentaAhorro = new Ahorro(3000);
+var cuentaCheques = new Cheques(6000);
+
+cuentaBasica.Depositar(200);
+cuentaAhorro.Retirar(500);
+cuentaCheques.EmitirCheque(400);
+
+Console.WriteLine($"Saldo cuenta básica: {cuentaBasica.Saldo}");
+Console.WriteLine($"Saldo cuenta de ahorro: {cuentaAhorro.Saldo}");
+Console.WriteLine($"Saldo cuenta de cheques: {cuentaCheques.Saldo}");
+Console.WriteLine($"Interés mensual cuenta de cheques: {cuentaCheques.CalcularInteresMensual()}");
+
+
