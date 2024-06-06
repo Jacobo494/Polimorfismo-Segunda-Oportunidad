0109aeb43142daa6158fbb6b69083f3ecd5e17ea
Alberto Jacobo Zamudio Chan <23470290@campeche.tecnm.mx>
2024-06-05 22:57:12 -0600

    Cuenta Basica. Deposita y retira dinero

---
+++ bancoPolimorfimo.ConsoleApp/CuentaBasica.cs
@@ -0,0 +1,21 @@
+﻿namespace bancoPolimorfimo.ConsoleApp
+{
+    public class CuentaBasica : ClaseBase
+    {
+        public CuentaBasica(decimal saldoM)
+        {
+            this.Saldo = saldoM;
+        }
+        public new void Depositar(decimal cantidad)
+        {
+            if (cantidad > 0)
+                Saldo += cantidad;
+        }
+
+        public override void Retirar(decimal cantidad)
+        {
+            if (cantidad <= Saldo)
+                this.Saldo -= cantidad;
+        }
+    }
+}
