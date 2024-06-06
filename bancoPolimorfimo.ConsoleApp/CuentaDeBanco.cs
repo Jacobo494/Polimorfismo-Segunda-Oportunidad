5e6b6f18d11bd6b7078d679aefab52d4e4831408
Alberto Jacobo Zamudio Chan <23470290@campeche.tecnm.mx>
2024-06-05 22:18:22 -0600

    Versión inicial. Solo incluye metodos

---
+++ bancoPolimorfimo.ConsoleApp/CuentaDeBanco.cs
@@ -0,0 +1,9 @@
+﻿namespace bancoPolimorfimo.ConsoleApp
+{
+    public interface CuentaDeBanco
+    {
+        decimal Saldo { get; }
+        void Depositar(decimal cantidad);
+        void Retirar(decimal cantidad);
+    }
+}
